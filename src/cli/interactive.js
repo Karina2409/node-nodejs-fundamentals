85f834e720e168cefc175f260521fd3990e1e0f7
Karina Kupryianava <karina.kupryianava@gmail.com>
2026-03-08 22:37:54 +0300

    feat: add solution to the task cli/interactive

--- src/cli/interactive.js
+++ src/cli/interactive.js
@@ -1,8 +1,37 @@
-const interactive = () => {
-  // Write your code here
-  // Use readline module for interactive CLI
-  // Support commands: uptime, cwd, date, exit
-  // Handle Ctrl+C and unknown commands
+import readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+const interactive = async () => {
+    try {
+        while (true) {
+            const command = (await rl.question('> ')).trim().toLowerCase();
+            switch (command) {
+                case 'uptime':
+                    const uptime = process.uptime().toFixed(2);
+                    console.log(`Uptime: ${uptime}s`);
+                    break;
+                case 'cwd':
+                    console.log(`${process.cwd()}`);
+                    break;
+                case 'date':
+                    const now = new Date();
+                    console.log(now.toISOString());
+                    break;
+                case 'exit':
+                    console.log('Goodbye!');
+                    return;
+                default:
+                    console.log(`Unknown command`);
+                    break;
+            }
+        }
+    } catch (err) {
+        console.log('\nGoodbye!');
+    } finally {
+        rl.close();
+    }
 };
 
-interactive();
+await interactive();
